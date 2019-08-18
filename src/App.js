13aef871d15ee7800dd507d6561f9b9cc2db92c4
Binben Wang <binbenwang1994@gmail.com>
2019-08-18 14:41:59 -0600

    Moved API calling from front-end to back-end

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai'
 import './App.css';
-
-const app = new Clarifai.App({
-  apiKey: '14f935bc91224b13a959949f295222de'
- });
  
 
 const ParticlesOptions = {
@@ -82,8 +77,16 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    console.log('click');
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+    .then(response => response.json())
     .then(response => {
       if (response) {
         fetch('http://localhost:3000/image', {
